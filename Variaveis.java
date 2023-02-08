237700042c51cd4d896b594d3ddd5c45baf8ea34
ramosyara <yara.fernanda.ptal@gmail.com>
2023-02-08 17:04:17 +0000

    variaveis salvas

--- Variaveis.java
+++ Variaveis.java
@@ -1,3 +1,26 @@
 public class Variaveis {
-    
-}
+    public static void main(String[] args) {
+
+        int idade=16;
+        byte quantidadeBolas=7;
+        char letra='Y';
+        boolean vivo=false;
+        double salario = 1302;
+        float salarioMenor = 1212;
+        short alunosPalmital = 10000;
+        long populacaoPalmital = 22000;
+        String nome;
+        nome="Yarinha, logo a mais bonita  ";
+
+        System.out.println("meu nome é: " +nome + "tenho: " +idade +" anos");
+        System.out.println(nome);
+         System.out.println(idade);
+         System.out.println(quantidadeBolas);
+         System.out.println(letra);
+         System.out.println(vivo);
+         System.out.println(salario);
+         System.out.println(salarioMenor);
+         System.out.println(alunosPalmital);
+         System.out.println(populacaoPalmital);
+   
+} }
