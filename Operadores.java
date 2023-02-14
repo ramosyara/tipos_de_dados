98a1d4e8d2b4d1d710365a8b5122be5673b5e4e4
ramosyara <yara.fernanda.ptal@gmail.com>
2023-02-14 14:43:00 +0000

    entrada de dados pelo teclado

--- Operadores.java
+++ Operadores.java
@@ -1,13 +1,26 @@
+ import java.util.Scanner;
+
 public class Operadores {
     public static void main(String[] yara){
-         int idade;
-         idade = 16;
-         idade++;
-         System.out.println("eu tenho " + idade + " anos");
-         idade+=10;
+
+        Scanner entrada = new Scanner (System.in);
+         int idade=0;
+         int numero1;
+         int numero2;
+         int soma;
+
+         System.out.println("digite sua idade");
+         idade=entrada.nextInt();
          System.out.println(idade);
 
 
+         System.out.println("digite um número: ");
+         numero1=entrada.nextInt();
+
+         System.out.println("digite um número: ");
+         numero2=entrada.nextInt();
+         soma=numero1+numero2;
+         System.out.println("a soma dos dois numeros são " + soma);
     }
    
 }
