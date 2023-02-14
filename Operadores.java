ba7eea47d56c81fe6dc2668a420bdf4a29340f60
ramosyara <yara.fernanda.ptal@gmail.com>
2023-02-14 14:14:29 +0000

    salvando operadores

--- Operadores.java
+++ Operadores.java
@@ -1,3 +1,13 @@
 public class Operadores {
+    public static void main(String[] yara){
+         int idade;
+         idade = 16;
+         idade++;
+         System.out.println("eu tenho " + idade + " anos");
+         idade+=10;
+         System.out.println(idade);
+
+
+    }
    
 }
