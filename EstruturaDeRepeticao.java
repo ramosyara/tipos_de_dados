dc0ed1d58f8080a9f4aef7b3e436fa404a7e8aef
ramosyara <yara.fernanda.ptal@gmail.com>
2023-02-15 17:17:10 +0000

    salvando repeticoes

--- EstruturaDeRepeticao.java
+++ EstruturaDeRepeticao.java
@@ -1,3 +1,41 @@
 public class EstruturaDeRepeticao {
+    public static void main (String args []) {
+        
+        /*for(int i=150; i<300;i++){
+            System.out.println(i);
+        } */
+
+        /*
+        int soma= 0;
+        for(int i=1; i<=1000;i++){
+        soma=soma+i;
+        
+        }
+        System.out.println(soma);
+        */
+
+        /* for (int i=3; i<100; i+=3){
+            System.out.println(i);
+        }*/
+
+      /*   int fatResult = 1;
+        for (int i=0;i>1;i--){
+           if(i ==0){
+            fatResult = 1;
+            break;
+           } else {
+            fatResult = i * fatResult;
+            
+           }
+        }
+System.out.println(fatResult); */
+
+
+
+
+
+
+
+    }
 
 }
